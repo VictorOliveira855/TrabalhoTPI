2ea456bcbd2eed5c73112e7e6551ad901508e3ac
VictorOliveira855 <43281771+VictorOliveira855@users.noreply.github.com>
2019-09-24 17:09:56 -0300

    Ae

---
+++ bd_sfstore.sql
@@ -0,0 +1,2 @@
+create database BD_SFSTORE;
+use BD_SFSTORE;
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include_once './conexao.php';
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StillFrag-Store</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+            <div class="grid-x grid-padding-x">
+                <div class="large-12 cell">
+                    <h1>Os melhores jogos do Brasil</h1>
+                    </div>
+                </div>
+            <a href = acao.php>Jogos de ação</a><br>
+            <a href = corrida.php>Jogos de corrida</a><br>
+            <a href = freetoplay.php>Jogos de Free to play</a><br>
+            <a href = online.php>Jogos Online</a><br>
+            <a href = rpg.php>Jogos de RPG</a><br>
+            <a href = simulacao.php>Jogos de simulação</a><br>
+
+
+    </body>
+    </html>
